f1714f62b963c841c2abf8e62d3f78d2ad0e5300
Hildibjørg <hildibjorg94@gmail.com>
2024-04-08 14:46:27 +0200

    removed generated data and added new main

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref, onMounted, computed, watch } from 'vue'
+
+  const todos = ref([])
+  const name = ref('')
+
+  const input_content = ref('')
+  const input_category = ref(null)
+
+  const todos_asc = computed(() => todos.value.sort((a: any, b: any) => {
+    return b.createdAt - a.createdAt
+  }))
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+  })
+
+  onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+  })
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
 
-  <main>
-    <TheWelcome />
+    <section class="create-todos">
+      
+    </section>
   </main>
+  
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
